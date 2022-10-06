abcc56cf815d4e672c69dbcd8267d8230155f4ca
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-10-06 15:09:11 +0700

    up files

---
+++ src/CheckLeapYear.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CheckLeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+        int year = scanner.nextInt();
+        boolean isLeapYear = false;
+        boolean isDivisibleBy4 = year % 4 == 0;
+        boolean isDivisibleBy100 = year % 100 == 0;
+        boolean isDivisibleBy400 = year % 400 == 0;
+        if (isDivisibleBy4) {
+            if (isDivisibleBy100) {
+                if (isDivisibleBy400) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        if (isLeapYear) {
+            System.out.printf("%d is a leap year",year);
+        } else {
+            System.out.printf("%d is not a leap year",year);
+        }
+    }
+}
